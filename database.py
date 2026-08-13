6d105c14bd413641c001617412f81ccaf8702611
Ashen Renusha <renushaashen@gmail.com>
2026-08-13 11:31:07 +0530

    Created DB

--- database.py
+++ database.py
@@ -2,6 +2,8 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float
 # pyrefly: ignore [missing-import]
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+# pyrefly: ignore [missing-import]
+from sqlalchemy import ForeignKey 
 
 # Database URL
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:ashen2000@localhost:5432/inventory_db"
@@ -23,3 +25,42 @@ class Product(Base):
     name = Column(String, index=True)
     price = Column(Float)
     quantity = Column(Integer)
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    customer_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    phone = Column(String)
+    email = Column(String, unique=True, index=True)
+
+class Supplier(Base):
+    __tablename__ = "suppliers"
+
+    supplier_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    company = Column(String)
+    email = Column(String, unique=True, index=True)
+    phone = Column(String)
+    address = Column(String)
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    category_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    description = Column(String)
+
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    product_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Float)
+    quantity = Column(Integer)
+    
+    # Foreign Keys
+    category_id = Column(Integer, ForeignKey("categories.category_id"))
+    supplier_id = Column(Integer, ForeignKey("suppliers.supplier_id"))
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 # pyrefly: ignore [missing-import]
 from fastapi import FastAPI
 from database import Base, engine
+from database import engine, Base
+import database
 
 # Create all tables in the database on startup
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Inventory System API is running!"}
+
+# Me line eken thamai database eke tables tika hadanne
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
